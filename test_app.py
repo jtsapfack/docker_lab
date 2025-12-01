119e43c3eaca70b2d6ac802ff9c5a66458cc0817
JOSEPH <tsapfackfabrice@gmail.com>
2025-12-01 16:25:29 +0100

    Modifying the test_app file

--- test_app.py
+++ test_app.py
@@ -1,8 +1,17 @@
+#import pytest
+#from app import app
+
+#def test_index():
+#	tester=app.test_client.get('/')
+#	response=tester.get('/')
+#	assert response.status_code==200
+#	assert b"Hello, World!" in response.data
+
 import pytest
 from app import app
 
-def test_index():
-	tester=app.test_client.get('/')
-	response=tester.get('/')
-	assert response.status_code==200
-	assert b"Hello, World!" in response.data
+def test_hello():
+    tester = app.test_client()  # Notez les parenthèses !
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
