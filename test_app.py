0a943bc10c85957af1d8513d315d9bec7b0fc995
JOSEPH <tsapfackfabrice@gmail.com>
2025-12-02 10:18:45 +0100

    Modifying python files

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import app
 
 def test_index():
 	tester=app.test_client()
-	response=client.get('/')
+	response=tester.get('/')
 	assert response.status_code==200
     assert b"Hello, World!" in response.data
